7ed48f1a6e0d62e9f9ddc73a48f9de003cb381d6
Josh Hausotter <jhausotter@gmail.com>
2024-01-17 10:51:54 -0800

    Add sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://malkuchsyardandtreeservice.com/' : 'http://localhost:4321/';
 
-const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://malkuchsyardandtreeservice.com/'
-  : 'http://localhost:4321/'
 
 // https://astro.build/config
 export default defineConfig({
   site: baseURL,
-  integrations: []
-})
+  integrations: [sitemap()]
+});
